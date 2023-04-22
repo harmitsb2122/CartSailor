d73d0b97b4a98442c1289fa7e1a56d3d10aa87a4
harmitsb2122 <harmitsb@gmail.com>
2023-04-22 17:27:04 +0530

    frontend setup and basic routing

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import PageNotFound from "./pages/PageNotFound";
+const App = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
